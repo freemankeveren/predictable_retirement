5e3360555cf75f074ec7ecf89e4d299401fc5f01
Keveren Freeman <freemankeveren@gmail.com>
2017-08-11 10:11:28 -0600

    added blog

--- src/App.js
+++ src/App.js
@@ -230,6 +230,33 @@ class App extends Component {
             </Grid.Column>
           </Grid>
         </Segment>
+        <Segment basic>
+          <h1>Our Latest Blog Posts</h1>
+          <Grid columns={2} padded='vertically'>
+            <Grid.Column verticalAlign='middle'>
+              <h3>Why You Will Pay More in Taxes at Retirement with a 401(k)</h3>
+              <h4>Hundreds of people just like you have found a Predicatable Retirement <br />
+              with our system. Stay up to date with our valuable emails sent weekly.</h4>
+            </Grid.Column>
+            <Grid.Column verticalAlign='middle'>
+              <h3>Start Saving into Whole Life Insurance (And Stop Saving into a 401k)</h3>
+              <h4>Hundreds of people just like you have found a Predicatable Retirement <br />
+              with our system. Stay up to date with our valuable emails sent weekly.</h4>
+            </Grid.Column>
+          </Grid>
+          <Grid columns={2} padded='vertically'>
+            <Grid.Column verticalAlign='middle'>
+              <h3>Start Saving into Whole Life Insurance (And Stop Saving into a 401k)</h3>
+              <h4>Hundreds of people just like you have found a Predicatable Retirement <br />
+              with our system. Stay up to date with our valuable emails sent weekly.</h4>
+            </Grid.Column>
+            <Grid.Column verticalAlign='middle'>
+              <h3>Start Saving into Whole Life Insurance (And Stop Saving into a 401k)</h3>
+              <h4>Hundreds of people just like you have found a Predicatable Retirement <br />
+              with our system. Stay up to date with our valuable emails sent weekly.</h4>
+            </Grid.Column>
+          </Grid>
+        </Segment>
         <Segment secondary style={style.heroFront}>
           <Footer />
         </Segment>
