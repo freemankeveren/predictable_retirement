62c9ad49c53a506bacfad36bf3159e0d9aa1d851
Keveren Freeman <freemankeveren@gmail.com>
2017-08-23 22:35:12 -0600

    added content to income page

--- src/components/Income.js
+++ src/components/Income.js
@@ -2,14 +2,56 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Segment } from 'semantic-ui-react';
 
+const style = {
+
+  heroFront:{
+    borderRadius: '0',
+    boxShadow: 'none',
+    padding: '64px',
+    margin:'0',
+    backgroundColor: '#EAF4F4',
+    fontSize: '48px',
+    fontWeight: '200',
+  },
+
+  stop:{
+    borderRadius: '0',
+    boxShadow: 'none',
+    padding: '64px',
+    margin:'0',
+    fontSize: '48px',
+    fontWeight: '200',
+  },
+
+  h0:{
+    fontSize: '48px',
+    fontWeight: '200',
+  },
+}
+
+
 class Income extends Component {
   render() {
     return (
       <div>
-        <h1>Income</h1>
+        <Segment basic textAlign='center' style={style.heroFront}>
+          <h1 text-align='center' style={style.h0}>
+            Maximize Your Income.
+          </h1>
+        </Segment>
+        <Segment basic textAlign='center' style={style.stop}>
+          <h2 text-align='center'>
+            Stop the transfer of wealth away from you.
+          </h2>
+          <h4 text-align='center'>
+            Example Text
+          </h4>
+        </Segment>
       </div>
-    );
+
+    )
   }
 }
+
 
 export default Income
